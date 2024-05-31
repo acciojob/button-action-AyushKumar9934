f6e19d21e366b26cf8bfa119032fcacdbb4c029b
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-31 20:45:56 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [state,setState]=useState(false);
+  function showPara(){
+    setState(true);
+
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p className={state?"show":"hide"} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+     {
+      (!state)&&(<button onClick={showPara} id="click">Get Paragraph</button>)
+     }
+     
     </div>
   );
 }
